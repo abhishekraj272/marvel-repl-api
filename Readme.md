03b55e310aab539c693f6c6ede440bd12601d00a
Abhishek Raj <abhishekraj272@gmail.com>
2022-07-08 08:01:38 +0000

    feat: added readme

---
+++ Readme.md
@@ -0,0 +1,7 @@
+# Marvel Search Interface
+
+### Local Setup
+1. ```git clone git@github.com:abhishekraj272/marvel-repl-api.git```
+2. ```cd marvel-repl-api```
+3. ```go get .```
+4. ```go run main.go```
--- main.go
+++ main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"fmt"
 	"main/cli"
-  "github.com/joho/godotenv"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
   err := godotenv.Load()
-  if err != nil {
+  if err != nil  {
     fmt.Println("Error loading .env file")
     return;
   }
